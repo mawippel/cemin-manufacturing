44f53f6144d3ac2b2c4df25b98b7877aaaed4179
mawippel <mawippel2@hotmail.com>
2024-10-12 12:24:05 -0300

    add dashboard structure

---
+++ ui/src/app/dashboards/page.tsx
@@ -0,0 +1,10 @@
+import Layout from "@/components/layout";
+
+export default function ColaboradoresPage() {
+    return (
+        <Layout>
+          <h1 className="text-2xl font-bold">Dashboards</h1>
+          <p>render charts here</p>
+        </Layout>
+      );
+}
